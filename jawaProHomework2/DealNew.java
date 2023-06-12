1e8c25896224606fc643d2291de7713b1f599a4f
Alexander Goranski <126571657+AlexgorAlexgor@users.noreply.github.com>
2023-06-12 22:48:44 +0200

    JavaProHomework2
    Домашнее задание:
    сделать рефактор кода, разнести логику в отдельные классы, enums, методы.

---
+++ jawaProHomework2/DealNew.java
@@ -0,0 +1,8 @@
+package jawaProHomework2;
+
+public class DealNew {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+}
---
+++ jawaProHomework2/Rank.java
@@ -0,0 +1,16 @@
+package jawaProHomework2;
+
+public enum Rank {
+    TWO,
+    TREE,
+    FOUR, FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE;
+}
